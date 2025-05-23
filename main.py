3f19ab43fbf67fe2ac72bd588df0d2219a744ec0
nb <nicollybrasil@hotmail.com>
2025-05-23 19:32:06 -0300

    formatação função

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import PyPDF2
 import os
 
-merger = PyPDF2.PdfMerger() #mesclador
+def mesclador_pdf():
+    merger = PyPDF2.PdfMerger() #mesclador
+    lista_arquivos = os.listdir("arquivos") #mostra os arquivos do diretorio
+    lista_arquivos.sort()
+    for arquivo in lista_arquivos:
+        if ".pdf" in arquivo:
+            merger.append(f"arquivos/{arquivo}")
+    return merger.write("mesclagem_final.pdf")
 
-lista_arquivos = os.listdir() #mostra os arquivos do diretorio
-
-for arquivo in lista_arquivos:
-    if ".pdf" in arquivo:
-        merger.append(f"arquivos/{arquivo}")
-
-merger.write("mesclagem_final.pdf")
+mesclador_pdf()
